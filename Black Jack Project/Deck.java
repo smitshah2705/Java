67dc7b43856059e697e690fd0660ff23f899315f
smitshah2705 <smit.shahb2026@stu.tes.tp.edu.tw>
2024-11-25 20:39:28 +0800

    Latest4

--- Black Jack Project/Deck.java
+++ Black Jack Project/Deck.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
     private String[] Suits = {"Diamond", "Heart", "Clubs", "Spades"};
     private String[] Rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", 
@@ -7,8 +9,29 @@ public class Deck {
 
     public Deck()
     {
-        ArrayList cards = new ArrayList <String> ();
-        
+        cards = new ArrayList <String> ();
+        for (int i = 0;i < Suits.length;i++)
+        {
+            for (int y = 0;i < Rank.length;y++)
+            {
+                cards.add(Rank[y] + " of " + Suits[i]);
+            }
+        }
+
+    }
+
+    public void shuffle()
+    {
+        Collections.shuffle(cards);
+    }
+
+    public String Drawcard()
+    {
+        if(cards.isEmpty())
+        {
+            return "No more cards left in the deck";
+        }
+        return cards.remove(0);
     }
 
 }
